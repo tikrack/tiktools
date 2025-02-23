fac411c2519d95d78ebf0e7d3903f739ff88d33c
tikrack <tikrackcode@gmail.com>
2025-02-24 02:01:32 +0330

    feat(github)

---
+++ app/Controllers/GithubController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers;
+
+class GithubController
+{
+    public static function repo() {
+        echo "ok";
+    }
+}
--- app/Controllers/StartController.php
+++ app/Controllers/StartController.php
@@ -7,17 +7,8 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class StartController
 {
-    /**
-     * @throws GuzzleException
-     */
     public function start(): void
     {
-        $client = new Client();
-
-        $response = $client->get("https://api.salamlang.ir/api/v1/code");
-
-        $result = $response->getBody()->getContents();
-
-        dd("this is a test");
+        GithubController::repo();
     }
 }
