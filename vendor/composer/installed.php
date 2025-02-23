fc4f8a647715e88ad324c208b4b746f91a40c393
tikrack <tikrackcode@gmail.com>
2025-02-24 00:24:46 +0330

    feat(flight: add flight)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd57417eed48ef3c2528b14f4ff3028b289f7cec2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd57417eed48ef3c2528b14f4ff3028b289f7cec2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'flightphp/core' => array(
+            'pretty_version' => 'v3.14.0',
+            'version' => '3.14.0.0',
+            'reference' => '2762daf4f4725eae784291ac4ef21b04a6863be6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../flightphp/core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mikecao/flight' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.0.2',
+            ),
+        ),
+    ),
+);
